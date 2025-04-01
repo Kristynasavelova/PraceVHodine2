50cf10b502b2ad242c12d1c479460a6b72817061
Kristynasavelova <kristyna.savelova@infis.cz>
2025-04-01 16:39:21 +0200

    Update PraceVHodine.py

--- PraceVHodine.py
+++ PraceVHodine.py
@@ -1,17 +1,20 @@
-predmety = ["Ucebni praxe", "Matematika", "Programovani", "Anglicky jazyk", "Cesky jazyk", "Pocitacove site", "Aplikacni software"]
+predmety = ["Ucebni praxe", "Matematika", "Programovani", "Anglicky jazyk", "Český jazyk", "Pocitacove site", "Aplikacni software"]
 
 DelkaPole = len(predmety)
 print(DelkaPole)
 
 for vypis in predmety:
-    print(vypis)
+    print(vypis)  
 
 DalsiPredmet = str(input("zadejte jeden předmět,který v seznamu ještě není: "))
-pridani = predmety.append(DalsiPredmet)
+predmety.append(DalsiPredmet)
 
-Odstranenni = predmety.remove("Cesky jazyk")
+if "Český jazyk" in predmety:
+  predmety.remove("Český jazyk")
+else:
+   print("předmět Český jakzyk není v seznamu")
 
-serazeni = predmety.sort
-
-obracenePoradi = predmety.reverse
-print(obracenePoradi)
+predmety.sort()
+print(predmety)
+predmety.reverse()
+print(predmety)
